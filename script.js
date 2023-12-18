0e8a8332c5a84f221b227d4af4d7d9ea2dd71e17
Md-Shahzadah-Sattar <148210694+Md-Shahzadah-Sattar@users.noreply.github.com>
2023-12-18 14:02:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-const extendHex = (shortHex) => {
-  // write your code here
-};
+function extendHex(shortHex) {
+  // Remove the '#' symbol if present
+  shortHex = shortHex.replace('#', '');
+
+  // Ensure the input is a valid short hex code
+  if (!/^[0-9a-fA-F]{3}$/.test(shortHex)) {
+    throw new Error('Invalid short hex code');
+  }
+
+  // Expand short hex to full hex
+  const fullHex = shortHex
+    .split('')
+    .map(char => char.repeat(2))
+    .join('');
+
+  // Add '#' prefix to the full hex code
+  return `#${fullHex}`;
+}
+
+// Test cases
+console.log(extendHex("#abc"));  // Output: "#aabbcc"
+console.log(extendHex("abc"));   // Output: "#aabbcc"
+console.log(extendHex("#AbC"));  // Output: "#AABBCC"
+console.log(extendHex("#f09"));  // Output: "#ff0099"
+
 
 // Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+// const shortHex = prompt("Enter Short Hex.");
+// alert(extendHex(shortHex));
